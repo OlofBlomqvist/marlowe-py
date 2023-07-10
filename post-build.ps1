3985adb3e2335959d6097adc14485aa4dab3927a
Olof Blomqvist <olof@twnet.se>
2023-07-10 13:19:48 +0200

    ci

---
+++ post-build.ps1
@@ -0,0 +1,4 @@
+# Triggered for the windows build in github ci
+
+pip install maturin
+maturin build --bindings pyo3 --release
--- post-build.sh
+++ post-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Triggered by github CI for osx and linux builds
 
 pip install maturin
 maturin build --bindings pyo3 --release
